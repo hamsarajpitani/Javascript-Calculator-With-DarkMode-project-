3f326d7daecc578be666d6df645986aa261701a6
PitaniHamsaraj <33552374+hamsarajpitani@users.noreply.github.com>
2020-07-08 12:57:56 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -43,3 +43,12 @@ const toggleSwitch = document.querySelector(
     }
 
   }
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
